68d24e2773d4edd3b256ab87122ef232c2b1c93e
Shawn <shosteam@jottnet.de>
2025-07-19 20:58:57 +0200

    TileMap und Player Animations

--- MagicMushroom2D/Assets/Scripts/Player.cs
+++ MagicMushroom2D/Assets/Scripts/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -19,6 +21,17 @@ public class Player : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("IsWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
